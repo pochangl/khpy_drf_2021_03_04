b75551932b253d972b546f74328f3464e89013e9
Pochang Lee <pochanglee2008@gmail.com>
2021-03-02 13:48:56 +0800

    add more setup test

--- mysite/poll/tests/test_step1_setup.py
+++ mysite/poll/tests/test_step1_setup.py
@@ -1,3 +1,4 @@
+from os import path
 from django.conf import settings
 from django.test import TestCase
 
@@ -5,3 +6,9 @@ from django.test import TestCase
 class TestSetup(TestCase):
     def test_apps(self):
         assert 'poll' in settings.INSTALLED_APPS, 'mysite/settings.py 的 INSTALLED_APP 裡沒有 poll package'
+
+    def test_model_file(self):
+        assert path.exists('poll/models.py'), 'poll/models.py 沒有定義'
+
+    def test_migration_file(self):
+        assert path.exists('poll/migrations/0001_initial.py'), '還沒有跑 python manage.py makemigrations poll'
