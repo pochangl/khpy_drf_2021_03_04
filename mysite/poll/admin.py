101aae2dad8aefc11809b497490e970e142a02b4
Pochang Lee <pochanglee2008@gmail.com>
2021-03-04 14:19:23 +0800

    add db data and admin

--- mysite/poll/admin.py
+++ mysite/poll/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Question, Choice
+
 
 # Register your models here.
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question_text')
+
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question', 'choice_text')
