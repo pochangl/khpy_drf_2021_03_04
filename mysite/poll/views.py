799a076af0d19f67bddaada2a130951bc8c32f24
Pochang Lee <pochanglee2008@gmail.com>
2021-03-04 14:35:16 +0800

    cbs

--- mysite/poll/views.py
+++ mysite/poll/views.py
@@ -1,39 +1,44 @@
 import json
 from django.http import HttpResponse, QueryDict
 from django.utils.timezone import now
+from django.views import View
 from .models import Question, Choice
 
 
-def question(request, pk=None):
-    if request.method == 'POST':
+class QuestionAPIView(View):
+    def post(self, request):
         text = json.loads(request.body).get('question_text')
         if len(text) > 200:
             return HttpResponse(status=400)
         Question.objects.create(question_text=text, pub_date=now())
         return HttpResponse()
-    elif request.method == 'PUT':
+
+    def put(self, request, pk):
         text = json.loads(request.body).get('question_text')
         if len(text) > 200:
             return HttpResponse(status=400)
         Question.objects.filter(pk=pk).update(question_text=text)
         return HttpResponse()
-    elif request.method == 'DELETE':
+
+    def delete(self, request, pk):
         Question.objects.filter(pk=pk).delete()
         return HttpResponse()
-    try:
-        question = Question.objects.get(pk=pk)
-    except Question.DoesNotExist:
-        return HttpResponse(status=404)
 
-    data = dict(
-        id=question.id,
-        question_text=question.question_text,
-        choices=list(
-            dict(
-                id=choice.id,
-                choice_text=choice.choice_text,
-            )
-            for choice in Choice.objects.all()
-        ),
-    )
-    return HttpResponse(json.dumps(data))
+    def get(self, request, pk):
+        try:
+            question = Question.objects.get(pk=pk)
+        except Question.DoesNotExist:
+            return HttpResponse(status=404)
+
+        data = dict(
+            id=question.id,
+            question_text=question.question_text,
+            choices=list(
+                dict(
+                    id=choice.id,
+                    choice_text=choice.choice_text,
+                )
+                for choice in Choice.objects.all()
+            ),
+        )
+        return HttpResponse(json.dumps(data))
